576f858d83a524e20a9c07eef43f31197d380407
LEE JUN HEE <wnsgml351@naver.com>
2024-06-17 22:38:22 +0900

    init

---
+++ src/PostDetail.jsx
@@ -0,0 +1,41 @@
+import { fetchComments } from "./api";
+import { useQuery } from "@tanstack/react-query";
+import "./PostDetail.css";
+
+export function PostDetail({ post }) {
+  // replace with useQuery
+  // const data = [];
+
+  const { data, isError, error, isLoading } = useQuery({
+    queryKey: ["comments"],
+    queryFn: () => fetchComments(post.id),
+    staleTime: 2000, // 2 seconds
+  });
+  if (isLoading) {
+    return <h3>Loading!</h3>;
+  }
+  if (isError) {
+    return (
+      <>
+        <h3>Error</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
+
+  console.log("data: ", data)
+
+  return (
+    <>
+      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <button>Delete</button> <button>Update title</button>
+      <p>{post.body}</p>
+      <h4>Comments</h4>
+      {data.map((comment) => (
+        <li key={comment.id}>
+          {comment.email}: {comment.body}
+        </li>
+      ))}
+    </>
+  );
+}
